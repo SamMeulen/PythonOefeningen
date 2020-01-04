045a4f62f42c32f14b1676e04f91603c391213d4
SamMeulen <sam.meulen@vdabcampus.be>
2020-01-04 19:53:27 +0100

    strings

--- strings.py
+++ strings.py
@@ -1,2 +1,23 @@
-a = "hello"
+""" a = "hello"
 print(a[1])
+
+b = "world"
+print(b[0:3])
+
+length = len(b)
+print(b[1:length]) """
+
+txt = "The rain in Spain stays mainly in the plain"
+x = "ain" in txt
+print(x)
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+
+quantity = 3
+itemno = 14
+price = 23.45
+myorder= "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
